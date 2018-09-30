1f59a0dd721d20b7e8569a267100f87bb51912aa
Mohammed <mohammed@refam.io>
2018-10-01 01:09:18 +0300

    Configure Styled Components for SSR

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,17 @@
 import Document, { Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
   render() {
+    const sheet = new ServerStyleSheet()
+    const main = sheet.collectStyles(<Main />)
+    const styleTags = sheet.getStyleElement()
+
     return (
-      <html lang={`en`}>
-        <Head>
-        </Head>
+      <html lang='en'>
+        <Head>{styleTags}</Head>
         <body>
-          <Main />
+          <div className='root'>{main}</div>
           <NextScript />
         </body>
       </html>
