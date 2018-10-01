9bc13cca58c39ecea9e53565d5727914c2c62243
Mohammed <mohammed@refam.io>
2018-10-01 16:51:57 +0300

    Animate header divider

--- components/header.js
+++ components/header.js
@@ -1,25 +1,42 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { Flex, Box, Heading, Card } from 'rebass'
 import { meta } from '../constants'
 
 const Badge = props =>
   <Card {...props} as='h4' color='black' bg='cyan' p={1} borderRadius={4} />
 
-const Divider = styled(Box)`
-  & { /* '&' to override modern-normalize 'hr' element class */
-    border: 0;
-    height: 1px;
+// Bar animation bootstraped from:
+// https://github.com/rebassjs/grid/blob/master/docs/components.js
+const gradient = (n, from, to) => `linear-gradient(${n}deg, ${from}, ${to})`
+
+const grow = keyframes`
+  from {
+    transform: scaleX(0);
+  }
+  to {
+    transform: scaleX(1);
   }
 `
 
-Divider.defaultProps = {
-  as: `hr`,
-  bg: `magenta`,
-}
+const Divider = styled(Box)`
+  & {
+    margin: 0;
+    border: 0;
+    height: 3px;
+    background-image: ${gradient(90, `cyan`, `magenta`)};
+    transform-origin: 0 0;
+    animation-name: ${grow};
+    animation-duration: 1s;
+    animation-timing-function: ease-out;
+    animation-fill-mode: forwards;
+  }
+`
+
+Divider.defaultProps = { as: `hr` }
 
 const Header = () =>
   <Box my={3}>
-    <Flex alignItems='flex-end' justifyContent='space-between'>
+    <Flex py={2} alignItems='flex-end' justifyContent='space-between'>
       <Heading as='h1' fontSize={[5, 6]}>{meta.title}</Heading>
       <Badge my={0}>Demo</Badge>
     </Flex>
