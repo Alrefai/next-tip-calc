08ee04c9e63ac6aa30cd6c37fc360581165863fb
Mohammed <mohammed@refam.io>
2018-10-08 15:43:16 +0300

    Initial App design

--- components/calculator.js
+++ components/calculator.js
@@ -1,11 +1,25 @@
-import { Box } from 'rebass'
+import { Box, Card, Flex, Text } from 'rebass'
 import { func, object } from 'prop-types'
+import { Total } from './total'
+import { Bill } from './bill'
+import { Tip } from './tip'
 
 const Calculator = ({
   dispatch,
   model: { amount, tipPercentage, tip, total } = {}
 }) =>
-  <Box>
+  <Box width={1}>
+    <Total {...{ total }} />
+    <Card width={1} bg='#333' borderRadius={15}>
+      <Flex flexDirection={[`column`, `row`]} alignItems='center'>
+        <Box width={1}>
+          <Bill {...{ amount }} />
+        </Box>
+        <Box width={1}>
+          <Tip {...{ tip }} />
+        </Box>
+      </Flex>
+    </Card>
   </Box>
 
 Calculator.propTypes = {
