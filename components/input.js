2c3bbdaa6bf8ba8e5ea5b53378921959189ef79b
Mohammed <mohammed@refam.io>
2018-10-08 15:33:18 +0300

    Refactor Input component with Flex

--- components/input.js
+++ components/input.js
@@ -1,9 +1,9 @@
 import debounce from 'lodash.debounce'
 import styled from 'styled-components'
-import { Box, Card, Text } from 'rebass'
-import { exact, oneOfType, string, number, object } from 'prop-types'
+import { Box, Flex, Card, Text } from 'rebass'
+import { exact, oneOfType, string, number, node, object } from 'prop-types'
 
-const handleChage = debounce(input => console.log(input), 500)
+const handleChage = debounce(input => console.log(input), 1000)
 
 const Label = styled(Text)`
   display: block;
@@ -14,12 +14,16 @@ const Field = styled(Card)`
 `
 
 export const Input = ({
-  label,
+  children,
   value,
   labelAttrs,
   inputAttrs,
   onChange = e => handleChage(e.target.value),
-  rebassBoxProps: { width: boxWidth = 1, ...restRebassBoxProps } = {},
+  rebassFlexProps: {
+    width: FlexWidth = 1,
+    flexWrap = `wrap`,
+    alignItems = `center`,
+    ...restRebassFlexProps } = {},
   rebassLabelProps: {
     fontSize: labelFontSize = [2, 3, 4],
     ...restRebassLabelProps,
@@ -31,38 +35,47 @@ export const Input = ({
     borderRadius = 4,
     ...restRebassFieldProps,
   } = {},
-}) =>
-  <Box width={boxWidth} {...restRebassBoxProps}>
-    {!label ? []
-    : <Label
-        as='label'
-        fontSize={labelFontSize}
-        {...{...labelAttrs, ...restRebassLabelProps }}
-      >
-        {label}
-      </Label>
-    }
-    <Field
-      as='input'
-      width={fieldWidth}
-      {...{
-        value,
-        onChange,
-        placeholder,
-        border,
-        borderRadius,
-        ...inputAttrs,
-        ...restRebassFieldProps,
-      }}
-    />
-  </Box>
+}) => {
+  const fieldProps = {
+    ...inputAttrs,
+    ...restRebassFieldProps,
+    as: `input`,
+    width: fieldWidth,
+    value,
+    onChange,
+    placeholder,
+    border,
+    borderRadius,
+  }
+
+  const flexProps = {
+    ...restRebassFlexProps,
+    width: FlexWidth,
+    flexWrap,
+    alignItems,
+  }
+
+  const labelProps = {
+    ...labelAttrs,
+    ...restRebassLabelProps,
+    as: `label`,
+    fontSize: labelFontSize,
+  }
+
+  return !children
+    ? <Field {...fieldProps} />
+    : <Flex {...flexProps}>
+        <Label {...labelProps}>{children}</Label>
+        <Field {...fieldProps}/>
+      </Flex>
+}
 
 Input.propTypes = {
-  label: string,
+  children: node,
   value: oneOfType([string, number]),
   labelAttrs: object,
   inputAttrs: object,
-  rebassBoxProps: exact({ ...Box.propTypes }),
+  rebassFlexProps: exact({ ...Box.propTypes, ...Flex.propTypes }),
   rebassLabelProps: exact({ ...Box.propTypes, ...Text.propTypes }),
   rebassFieldProps: exact({ ...Box.propTypes, ...Card.propTypes }),
 }
