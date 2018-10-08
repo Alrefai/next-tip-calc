c6eeebbec233cc885fa1efb62b0d0cffd8754109
Mohammed <mohammed@refam.io>
2018-10-08 15:41:50 +0300

    Create Bill component

---
+++ components/bill.js
@@ -0,0 +1,34 @@
+import { Heading } from 'rebass'
+import { number, func } from 'prop-types'
+import { Input } from './input'
+import { Bar } from './bar'
+
+const inputProps = {
+  labelAttrs: { htmlFor: `bill-amount` },
+  inputAttrs: { id: `bill-amount` },
+  rebassFlexProps: {
+    mb: [1, 0],
+    p: 2,
+    flexDirection: `column-reverse`,
+  },
+  rebassLabelProps: {
+    width: 1,
+  },
+  rebassFieldProps: {
+    p: 0,
+    fontSize: 4,
+    color: `inherit`,
+    bg: `inherit`,
+  }
+}
+
+export const Bill = ({ amount = 125, onChange }) =>
+  <Input value={amount} {...{ ...inputProps, onChange }}>
+    <Bar as='hr'/>
+    <Heading as='h3' pt={1} fontSize={3} textAlign='right'>Bill Amount</Heading>
+  </Input>
+
+Bill.propTypes = {
+  amount: number,
+  onChange: func,
+}
