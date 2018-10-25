1225c50e30d4ac6962f907f322be262a884c1dc8
Mohammed <mohammed@refam.io>
2018-10-25 16:59:49 +0300

    Refactor functional components syntax

--- components/calculator.js
+++ components/calculator.js
@@ -3,13 +3,14 @@ import { func, object } from 'prop-types'
 import { Total } from './total'
 import { CalculatorInput } from './calculatorInput'
 
-const Calculator = ({ dispatch, model: { total, ...props } = {} }) =>
+const Calculator = ({ dispatch, model: { total, ...props } = {} }) => (
   <Box width={1}>
     <Total {...{ total }} />
     <Card width={1} bg='#333' borderRadius={15}>
       <CalculatorInput {...{ dispatch, ...props }} />
     </Card>
   </Box>
+)
 
 Calculator.propTypes = {
   dispatch: func,
