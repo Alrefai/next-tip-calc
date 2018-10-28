cbe6719d80148d80b825cc5c4893fe2b614f249a
Mohammed <mohammed@refam.io>
2018-10-28 07:08:32 +0300

    Refactor TipPercentage with function composition

--- components/tipPercentage.js
+++ components/tipPercentage.js
@@ -1,43 +1,40 @@
+import { curry, map, pipe } from 'ramda'
 import { Card, Flex, Text } from 'rebass'
 import { func, number } from 'prop-types'
 import { Circle } from './circle'
 import { PERCENTAGES } from '../constants'
 
+const handleClick = value => () => console.log(value)
 const tipPercentageProps = {
   variant: `primary`,
   width: 1,
   p: 1,
   borderRadius: 15,
 }
-
-const tipCircleProps = (currentPercentage, percentage, onClick) => ({
+const tipCircleProps = (currentPercentage, onClick, percentage) => ({
   key: percentage + `-percent`,
   type: `button`,
   as: `button`,
   m: 1,
   p: 2,
   color: `color`,
-  bg: currentPercentage === percentage ? `secondary` : `black`,
+  bg: currentPercentage === percentage ? `secondary` : `background`,
   border: 0,
   onClick: onClick(percentage),
 })
-
-const TipCircle = ({
-  tipPercentage,
-  onClick,
-}) => PERCENTAGES.map(percentage => (
-  <Circle {...tipCircleProps(tipPercentage, percentage, onClick)}>
+const tipCircle = curry((tipPercentage, onClick, percentage) => (
+  <Circle {...tipCircleProps(tipPercentage, onClick, percentage)}>
     <Text fontSize={4}>{percentage}%</Text>
   </Circle>
 ))
 
-export const TipPercentage = ({ tipPercentage = 15, onClick }) => (
-  <Card {...tipPercentageProps}>
-    <Flex justifyContent='space-evenly' alignItems='center'>
-      <TipCircle {...{ tipPercentage, onClick }} />
-    </Flex>
-  </Card>
-)
+export const TipPercentage = ({
+  tipPercentage = 15, onClick = handleClick
+}) => pipe(
+  map(tipCircle(tipPercentage, onClick)),
+  __ => <Flex justifyContent='space-evenly' alignItems='center'>{__}</Flex>,
+  __ => <Card {...tipPercentageProps}>{__}</Card>
+)(PERCENTAGES)
 
 TipPercentage.propTypes = {
   tipPercentage: number,
