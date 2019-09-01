2e1422e118b87ea7e55e3dfe8111f134a8872831
Mohammed <mohammed@refam.io>
2019-09-01 15:12:22 +0300

    Create context StoreProvider

--- context/index.js
+++ context/index.js
@@ -1,4 +1,20 @@
-import { createContext } from 'react'
+import { createContext, useReducer } from 'react'
+import { node } from 'prop-types'
+import { initModel } from '../constants'
+import reducer from '../reducers'
 
 export const DispatchContext = createContext()
 export const ModelContext = createContext()
+
+export const StoreProvider = ({ children }) => {
+  const [model, dispatch] = useReducer(reducer, initModel)
+  return (
+    <ModelContext.Provider value={model}>
+      <DispatchContext.Provider value={dispatch}>
+        {children}
+      </DispatchContext.Provider>
+    </ModelContext.Provider>
+  )
+}
+
+StoreProvider.propTypes = { children: node }
