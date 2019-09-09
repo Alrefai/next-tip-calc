e2c9770e3751e9ab8c67d24efc523856f549d357
Mohammed <mohammed@refam.io>
2019-09-09 18:17:13 +0300

    Refactor header component

--- components/header.js
+++ components/header.js
@@ -8,17 +8,6 @@ const { title } = meta
 
 const modes = [`neon`, `dracula`, `eclectus`]
 
-const buttonProps = (mode, setVisible) => ({
-  title: `Change color mode to ${mode}`,
-  variant: `transparent`,
-  my: 1,
-  p: 1,
-  size: 32,
-  sx: { borderRadius: `circle` },
-  onMouseEnter: () => setVisible(true),
-  onMouseLeave: () => setVisible(false),
-})
-
 const svgProps = {
   viewBox: `0 0 32 32`,
   width: `24`,
@@ -44,6 +33,8 @@ const pathProps = {
   `,
 }
 
+// Theme switcher icon was bootstraped from:
+// https://github.com/rebassjs/rebass/blob/master/packages/docs/src/components/header.js
 const Dot = () => (
   <svg {...svgProps}>
     <circle {...circleProps} />
@@ -51,46 +42,56 @@ const Dot = () => (
   </svg>
 )
 
-const modeProps = visible => ({
-  as: `p`,
-  ml: `auto`,
-  px: 2,
-  color: `primary`,
-  sx: {
-    display: !visible && `none`,
-    animationName: `fadeIn`,
-    animationDuration: `1.5s`,
-    animationTimingFunction: `ease-in`,
-  },
-})
-
-const headingProps = visible => ({
-  as: `h1`,
-  fontSize: 5,
-  opacity: visible ? 0 : 1,
-  sx: {
-    position: visible && `absolute`,
-    overflow: visible && `hidden`,
-    animationName: visible ? `fadeOut` : `fadeIn`,
-    animationDuration: `0.5s`,
-    animationTimingFunction: `ease-in`,
-  },
-})
-
-// Theme switcher icon was bootstraped from:
-// https://github.com/rebassjs/rebass/blob/master/packages/docs/src/components/header.js
 const Header = () => {
   const [visible, setVisible] = useState(false)
   const [mode, setMode] = useColorMode()
   const nextModeIndex = (modes.indexOf(mode) + 1) % modes.length
   const nextMode = modes[nextModeIndex]
-  const onClick = () => setMode(nextMode)
+
+  const headingProps = {
+    as: `h1`,
+    fontSize: 5,
+    opacity: visible ? 0 : 1,
+    sx: {
+      position: visible && `absolute`,
+      overflow: visible && `hidden`,
+      animationName: visible ? `fadeOut` : `fadeIn`,
+      animationDuration: `1s`,
+      animationTimingFunction: visible ? `ease-out` : `ease-in`,
+    },
+  }
+
+  const modeProps = {
+    as: `p`,
+    ml: `auto`,
+    px: 2,
+    color: `primary`,
+    sx: {
+      display: !visible && `none`,
+      animationName: `fadeIn`,
+      animationDuration: `2s`,
+      animationTimingFunction: `ease-in`,
+    },
+  }
+
+  const buttonProps = {
+    title: `Change color mode to ${nextMode}`,
+    variant: `transparent`,
+    my: 1,
+    p: 1,
+    size: 32,
+    sx: { borderRadius: `circle` },
+    onMouseEnter: () => setVisible(true),
+    onMouseLeave: () => setVisible(false),
+    onClick: () => setMode(nextMode),
+  }
+
   return (
     <Box as='header' maxWidth={512} mx='auto' px={2} mb={1}>
       <Flex mt={2} py={2} justifyContent='space-between' alignItems='center'>
-        <Heading {...headingProps(visible)}>{title}</Heading>
-        <Box {...modeProps(visible)}>{mode}</Box>
-        <Button {...{ ...buttonProps(nextMode, setVisible), onClick }}>
+        <Heading {...headingProps}>{title}</Heading>
+        <Box {...modeProps}>{mode}</Box>
+        <Button {...buttonProps}>
           <Dot />
         </Button>
       </Flex>
