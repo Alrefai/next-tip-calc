6a97e95c9fc18b9d0ff637013329e24bfd2cd9b1
Mohammed <mohammed@refam.io>
2019-09-14 21:22:56 +0300

    Customize card shadow in dracula theme mode

--- components/calculatorInput.js
+++ components/calculatorInput.js
@@ -1,4 +1,5 @@
 import { Flex } from 'rebass'
+import { useColorMode } from 'theme-ui'
 import { Bill } from './bill'
 import { Tip } from './tip'
 import { TipPercentage } from './tipPercentage'
@@ -12,16 +13,19 @@ const tipForm = [
   <TipInput key='tip-input' />,
 ]
 
-const flexProps = {
-  variant: `card`,
-  width: 1,
-  p: 0,
-  bg: `muted`,
-  flexDirection: [`column`, `row`],
-  sx: { borderRadius: `card` },
-}
-
 export const CalculatorInput = () => {
   const { showTipForm } = useModel()
+  const [mode] = useColorMode()
+  const boxShadow = ({ withShadow }) =>
+    withShadow({ dracula: { preSet: `0 0 8px`, alpha: 0.35 } }[mode])
+
+  const flexProps = {
+    variant: `card`,
+    width: 1,
+    p: 0,
+    bg: `muted`,
+    flexDirection: [`column`, `row`],
+    sx: { boxShadow, borderRadius: `card` },
+  }
   return wrapWith(Flex, flexProps, !showTipForm ? billForm : tipForm)
 }
