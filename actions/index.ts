52d6de74a54d9d081fbc1c9f35c5dfe785306487
Mohammed <mohammed@refam.io>
2019-09-23 13:15:37 +0300

    Refactor actions/index.ts with TypeScript

--- actions/index.ts
+++ actions/index.ts
@@ -2,19 +2,28 @@ export const ACTIONS = {
   AMOUNT_INPUT: `AMOUNT_INPUT`,
   SHOW_TIP_INPUT: `SHOW_TIP_INPUT`,
   TIP_INPUT: `TIP_INPUT`,
+} as const
+
+type Message = keyof typeof ACTIONS
+
+interface Action {
+  <T>(x: T): {
+    type: Message
+    readonly [key: string]: T | Message
+  }
 }
 
-export const amountInputAction = amount => ({
+export const amountInputAction: Action = amount => ({
   type: ACTIONS.AMOUNT_INPUT,
   amount,
 })
 
-export const showTipFormAction = showTipForm => ({
+export const showTipFormAction: Action = showTipForm => ({
   type: ACTIONS.SHOW_TIP_INPUT,
   showTipForm,
 })
 
-export const tipInputAction = tipPercentage => ({
+export const tipInputAction: Action = tipPercentage => ({
   type: ACTIONS.TIP_INPUT,
   tipPercentage,
 })
