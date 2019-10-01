6128aa10770cfe1066360adbf069875bcaefd4fe
Mohammed <mohammed@refam.io>
2019-10-01 19:51:58 +0300

    refactor(wrappers): add typescript types

--- components/wrappers.tsx
+++ components/wrappers.tsx
@@ -1,36 +1,11 @@
-/* eslint-disable import/no-unused-modules */
-import {
-  addIndex,
-  converge,
-  curry,
-  map,
-  pipe,
-  unapply,
-  identity,
-  partial,
-} from 'ramda'
+import { curry } from 'ramda'
 
-const mapIndexed = addIndex(map)
+type Wrap = (
+  Tag: React.ElementType,
+  props: {},
+  children: JSX.Element | readonly JSX.Element[],
+) => JSX.Element
 
-const getKeyId = (props, i) => {
-  const id = props.id && i !== null ? props.id[i] : props.id
-  const key = i !== null && ((props.key && props.key[i]) || id || `id-${i}`)
-  return id && key ? { id, key } : (id && { id }) || (key && { key }) || {}
-}
-
-export const wrapWith = curry((Tag, props, children, index = null) => (
-  <Tag {...{ ...props, ...getKeyId(props, index) }}>{children}</Tag>
+export const wrapWith = curry<Wrap>((Tag, props, children) => (
+  <Tag {...props}>{children}</Tag>
 ))
-
-export const wrapAllWith = curry((Tag, tagProps, children, props) =>
-  pipe(
-    converge(unapply(identity), children),
-    wrapWith(Tag, tagProps),
-  )(props),
-)
-export const wrapEachWith = curry((Tag, tagProps, children, props) =>
-  pipe(
-    converge(unapply(identity), children),
-    mapIndexed(partial(wrapWith, [Tag, tagProps])),
-  )(props),
-)
