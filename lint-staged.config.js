f1e94dcef7af465191ed70f0e47d00051d629b89
Mohammed <mohammed@refam.io>
2019-10-04 01:34:00 +0300

    build(lint-staged): add type-check pre-commit task

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,9 @@
-{
-  "*.{js,jsx,ts,tsx}": ["eslint"],
-  "**/*.+(json|yml)": ["prettier --write", "git add"]
+/* eslint-disable import/no-unused-modules */
+module.exports = {
+  '**/*.ts{,x}': () => `tsc -p tsconfig.json --noEmit`,
+  '**/*.+(j|t)s{,x}': filenames => `eslint ${filenames.join(` `)}`,
+  '**/*.+(json|yaml)': filenames =>
+    filenames.map(
+      filename => `prettier --write '${filename}' && git add '${filename}'`,
+    ),
 }
