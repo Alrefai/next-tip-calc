cd56a8791d2c85118acb0836674d7326f98ca79c
Mohammed <mohammed@refam.io>
2019-10-03 16:21:10 +0300

    refactor(wrappers): refine type definitions

--- components/wrappers.tsx
+++ components/wrappers.tsx
@@ -3,8 +3,8 @@ import { curry } from 'ramda'
 type Wrap = (
   Tag: React.ElementType,
   props: {},
-  children: JSX.Element | readonly JSX.Element[],
-) => JSX.Element
+  children: React.ReactNode,
+) => React.ReactElement
 
 export const wrapWith = curry<Wrap>((Tag, props, children) => (
   <Tag {...props}>{children}</Tag>
