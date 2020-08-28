8e315c06e3abda07ef8f449b1a4c78d9ad78a1f4
Mohammed <mohammed@refam.io>
2020-08-28 22:43:11 +0300

    fix(reducer): handle bill amount input with proper logic
    Fixed reducer of bill amount onChange input handler to behave as expected when validating user
    input. Now, previous amount state will be returned when input is invalid.

--- context/index.tsx
+++ context/index.tsx
@@ -1,12 +1,15 @@
-import { FC, ReactNode, useReducer } from 'react'
-import { initModel } from '../constants'
+import { useReducer } from 'react'
+import { model } from '../constants'
 import { ModelProvider, DispatchProvider } from '../hooks'
 import { reducer } from '../reducers'
 
-export const StoreProvider: FC<ReactNode> = ({ children }) => {
-  const [model, dispatch] = useReducer(reducer, initModel)
+type Props = { readonly children: readonly JSX.Element[] | JSX.Element }
+
+export const StoreProvider = ({ children }: Props): JSX.Element => {
+  const [state, dispatch] = useReducer(reducer, model)
+
   return (
-    <ModelProvider value={model}>
+    <ModelProvider value={state}>
       <DispatchProvider value={dispatch}>{children}</DispatchProvider>
     </ModelProvider>
   )
